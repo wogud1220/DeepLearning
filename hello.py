798e6f64c530cbc626f4652874e8ad2bc104d973
wogud1220 <yoonwogud@gmail.com>
2025-10-16 11:28:25 +0900

    hello

---
+++ hello.py
@@ -0,0 +1,12 @@
+import torch
+
+a = torch.arange(12).reshape(3,4)
+b = a.transpose(0,1)      # view
+c = b.contiguous()        # 새 메모리
+
+a[0,0] = 999
+print("a:\n", a)
+print("b:\n", b)          # 같이 바뀜
+print("c:\n", c)          # 안 바뀜 (복사본)
+
+torch.permute(a, (0, 2, 1))
